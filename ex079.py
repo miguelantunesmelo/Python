09a2fcdfcd087c6e980ff9641323e1a6ac31d0b1
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-04-09 16:57:47 -0400

    new exercise

---
+++ ex079.py
@@ -0,0 +1,13 @@
+números = list()
+while True:
+    n = int(input('Digite um valor: '))
+    if n not in números:
+        números.append(n)
+        print('Valor adcionado com sucesso...')
+    else:
+        print('Valor duplicado! Não vou adcionar...')
+    r = str(input('Quer continuar? [S/N] '))
+    if r in 'Nn':
+        break
+print('-=' * 30)
+print(f'Você digitou os valores {números}')
