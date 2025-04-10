6f0d12cbbbfa03be625c37c0250a15371fc2e3ef
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-04-10 14:50:42 -0400

    up

---
+++ ex081.py
@@ -0,0 +1,8 @@
+valores = []
+while True:
+    valores.append(int(input('Gigite um valor: ')))
+    resp = str(input('Quer continuar? [S/N] '))
+    if resp in 'Nn':
+        break
+print('-=' * 30)
+print(f'Você digitou {len(valores)} elementos.') 
