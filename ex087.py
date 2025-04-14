a1f3331b1189f884f00c05774d684653175dcab1
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-04-14 16:12:11 -0400

    update

--- ex087.py
+++ ex087.py
@@ -7,6 +7,17 @@ print('-=' * 30)
 for l in range(0, 3):
     for c in range(0, 3):
         print(f'[{matriz[l][c]:^5}]', end='')
-        if matriz[l][c]
+        if matriz[l][c] % 2 == 0:
+            spar += matriz[l][c]
     print()
-print('-='  30)
+print('-=' * 30)
+print(f'A soma dos valores pares é {spar}')
+for l in range(0, 3):
+    scol += matriz[l][2]
+print(f' A soma dosvalores da terecira coluna é {scol}')
+for c in range(0 ,3):
+    if c == 0:
+        main = matriz[1][c]
+    elif matriz[1] [c] > mai:
+        mai = matriz[1] [c]
+print(f'O maior valor da segunda linha é {mai}.')
