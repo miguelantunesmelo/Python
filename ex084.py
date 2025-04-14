0b58bb0c296f686b055e6efd013804d6b7ddb67c
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-04-14 14:31:42 -0400

    updates

--- ex084.py
+++ ex084.py
@@ -18,9 +18,10 @@ while True:
         break
 print('-=' * 30)
 print(f'Os dados foram {princ}')
-print(f'Ao todo, você cadastrou {len(princ)} pessoas.')
+print(f'Ao todo, você cadastrou {len(princ)} pessoas. Peso de ', end='')
 print(f'O maior peso foi de {mai}Kg.')
 for p in princ:
     if p[1] == mai:
-        print(f'{p[0]}')
+        print(f'{p[0]} ', end='')
+print()
 print(f'O menor peso foi de {men}Kg.')
