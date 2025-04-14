58255e2b3ec2b9ba8e86fa920a5488a6f560ee97
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-04-14 11:10:22 -0400

    up

--- ex084.py
+++ ex084.py
@@ -5,6 +5,7 @@ while True:
     temp.append(str(input('Nome: ')))
     temp.append(str(input('Peso: ')))
     princ.append(temp[:])
+    temp.clear
     resp = str(input('Quer continuar? [S/N] '))
     if resp in 'Nn':
         break
