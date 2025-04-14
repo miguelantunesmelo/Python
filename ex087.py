8a2de552852a637d900817025edc2b3da59d8b1d
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-04-14 16:03:11 -0400

    update

--- ex087.py
+++ ex087.py
@@ -1,4 +1,5 @@
 matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+spar = mai = scol = 0
 for l in range(0, 3):
     for c in range(0, 3):
         matriz[l] [c] = int(input(f'Digite um valor para [{l}, {c}]: '))
@@ -6,4 +7,6 @@ print('-=' * 30)
 for l in range(0, 3):
     for c in range(0, 3):
         print(f'[{matriz[l][c]:^5}]', end='')
+        if matriz[l][c]
     print()
+print('-='  30)
