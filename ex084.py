c15e26dbee9fb56815c17adc5a0c581ed86ae68f
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-04-14 11:22:35 -0400

    Update ex084.py

--- ex084.py
+++ ex084.py
@@ -3,7 +3,7 @@ princ = []
 mai = men = 0
 while True:
     temp.append(str(input('Nome: ')))
-    temp.append(str(input('Peso: ')))
+    temp.append(float(input('Peso: ')))
     if len(princ) == 0:
         mai = men = temp[1]
     else:
