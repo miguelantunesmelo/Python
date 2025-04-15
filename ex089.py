92f895a7acc60e37d1889f52e1be49d0f960e163
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-04-15 10:56:40 -0400

    update

--- ex089.py
+++ ex089.py
@@ -1,7 +1,9 @@
 ficha = list()
 
-nome = str('Nome: ')
-nota1 = str('Nota 1: ')
-nota2 = str('Nota 2: ')
+nome = str(input('Nome: '))
+nota1 = float(input('Nota 1: '))
+nota2 = float(input('Nota 2: '))
 media = (nota1 + nota2) / 2
 ficha.append([nome, [nota1, nota2], media])
+
+print(ficha)
