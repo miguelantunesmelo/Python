85f9143af2ea2e0f11b6a456e54160dee472e883
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-04-15 11:06:03 -0400

    update

--- ex089.py
+++ ex089.py
@@ -1,9 +1,18 @@
 ficha = list()
-
-nome = str(input('Nome: '))
-nota1 = float(input('Nota 1: '))
-nota2 = float(input('Nota 2: '))
-media = (nota1 + nota2) / 2
-ficha.append([nome, [nota1, nota2], media])
-
-print(ficha)
+while True:
+    nome = str(input('Nome: '))
+    nota1 = float(input('Nota 1: '))
+    nota2 = float(input('Nota 2: '))
+    media = (nota1 + nota2) / 2
+    ficha.append([nome, [nota1, nota2], media])
+    resp = str(input('Quer continuar? [S/N] '))
+    if resp in 'Nn':
+        break
+print('-=' * 30)
+print(f'{"No.":<4}{"NOME":<10}{"MÉDDIA":>8}')
+print('-' * 26)
+for i, a in enumerate(ficha):
+    print(f'{i:<4}{a[0]:<10}{a[2]:>8.1f}')
+while True:
+    print('-' * 35) 
+    opc = 
