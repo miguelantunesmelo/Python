4aca9ec43e30337793cd696efb1e8d9addcbaca6
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-04-15 16:15:39 -0400

    update

--- ex090.py
+++ ex090.py
@@ -1,3 +1,10 @@
 aluno = dict()
 aluno['nome'] = str(input('Nome: '))
 aluno['média'] = float(input(f'Média de {aluno["nome"]}: '))
+if aluno['média'] >= 7:
+    aluno['situação'] = 'Aprovado'
+elif 5 <= aluno['média'] < 7:
+    aluno['situação'] = 'Recuperação'
+else:
+    aluno['situação'] = 'Reprovado'
+print(aluno)
