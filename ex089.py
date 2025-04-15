ca9709599d23cc391315ef34f3616d9df3647540
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-04-15 11:11:22 -0400

    updates

--- ex089.py
+++ ex089.py
@@ -9,10 +9,16 @@ while True:
     if resp in 'Nn':
         break
 print('-=' * 30)
-print(f'{"No.":<4}{"NOME":<10}{"MÉDDIA":>8}')
+print(f'{"No.":<4}{"NOME":<10}{"MÉDIA":>8}')
 print('-' * 26)
 for i, a in enumerate(ficha):
     print(f'{i:<4}{a[0]:<10}{a[2]:>8.1f}')
 while True:
     print('-' * 35) 
-    opc = 
+    opc = int(input('Mostrar notas de qual aluno? (999 interrompe): '))
+    if opc == 999:
+        print('FINALIZANDO...')
+        break
+    if opc <= len(ficha) - 1:
+        print(f'Notas de {ficha[opc][0]} são {ficha[opc][1]}')
+print('<<< VOLTE SEMPRE! >>>')
