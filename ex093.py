2ae2db229efd14ffee4fe709b8c5b64a08f67afc
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-04-16 14:43:06 -0400

    update

--- ex093.py
+++ ex093.py
@@ -12,7 +12,7 @@ print('-=' * 30)
 for k, v in jogador.items():
         print(f'O campo {k} tem o valor {v}')
 print('-=' * 30)
-print(f'O ogador {jogador["nome"]} jogou {len(jogador["gols"])} partidas.')
+print(f'O jogador {jogador["nome"]} jogou {len(jogador["gols"])} partidas.')
 for i, v in enumerate(jogador['gols']):
       print(f'  => Na partida {i}, fez {v} gols.')
 print(f'Foi um total de {jogador["total"]} gols.')
---
+++ ex094.py
@@ -0,0 +1,21 @@
+galera = list()
+pessoa = dict()
+while True:
+    pessoa.clear()
+    pessoa['nome'] = str(input('Nome: '))
+    while True:
+        pessoa['sexo'] = str(input('Sexo: [M/F] ')).upper()[0]
+        if pessoa['sexo'] in 'MF':
+            break
+        print('ERRO! Por favor, digite apenas M ou F.')
+    pessoa['idade'] = int(input('Idade: '))
+    galera.append(pessoa.copy)
+    while True:
+        resp = str(input('Quer continuar? [S/N] ')).upper()[0]
+        if resp in 'SN':
+            break
+        print('ERRO! Responda apenas S ou N.')
+    if resp =='S':
+        break
+print('-=' * 30)
+print(pessoa)
