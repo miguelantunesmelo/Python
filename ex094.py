d0e1cca0a58d569ef9494b266e0eedb1d487db3e
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-04-16 15:03:19 -0400

    update

--- ex094.py
+++ ex094.py
@@ -1,5 +1,6 @@
 galera = list()
 pessoa = dict()
+soma = média = 0
 while True:
     pessoa.clear()
     pessoa['nome'] = str(input('Nome: '))
@@ -9,13 +10,29 @@ while True:
             break
         print('ERRO! Por favor, digite apenas M ou F.')
     pessoa['idade'] = int(input('Idade: '))
-    galera.append(pessoa.copy)
+    soma += pessoa['idade']
+    galera.append(pessoa.copy())  
     while True:
         resp = str(input('Quer continuar? [S/N] ')).upper()[0]
         if resp in 'SN':
             break
         print('ERRO! Responda apenas S ou N.')
-    if resp =='S':
+    if resp == 'N':  
         break
 print('-=' * 30)
-print(pessoa)
+print(f'A) Ao todo temos {len(galera)} pessoas cadastradas.')
+média = soma / len(galera)
+print(f'B) A média de idade é de {média:5.2f} anos.')
+print('C) As mulheres cadastradas foram ', end='')
+for p in galera:
+    if p['sexo'] == 'F':
+        print(f'{p["nome"]} ', end='')
+print()
+print('D) Lista das pessoas que estão acima da média: ')
+for p in galera:
+    if p['idade'] >= média:
+        print('  ', end='')
+        for k, v in p.items():
+            print(f'{k} = {v}; ', end='')
+        print()   
+print('<<< ENCERRADO >>>')
