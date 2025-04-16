6ff0a1bed4854dd0d5a1f173a97bd577ff3ea0a2
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-04-16 15:53:40 -0400

    update

--- ex095.py
+++ ex095.py
@@ -3,14 +3,28 @@ jogador = dict()
 partidas = list()
 
 while True:
+    jogador.clear()
     jogador['nome'] = str(input('Nome do jogador: '))
     tot = int(input(f'Quantas partidas {jogador["nome"]} jogou?'))
     for c in range(0, tot):
         partidas.append(int(input(f'  Quantos gol na partida {c}? ')))
     jogador['gols'] = partidas[:]
     jogador['total'] = sum(partidas)
-    resp = str(input())
-
+    time.append(jogador.copy())
+    while True:
+        resp = str(input('Quer continuar? [S/N] ')).upper()[0]
+        if resp in 'SN':
+             break
+        print('ERRO! Responda apenas S ou N.')
+    if resp == 'N':
+         break
+print('-' * 40)
+for k, v in enumerate(time):
+     print(f'{k:>3}', end='')
+     for d in v.values():
+        print(f'{str(d):<15}', end='')
+        print()
+print('-' * 40)
 print('-=' * 30)
 print(jogador)
 print('-=' * 30)
