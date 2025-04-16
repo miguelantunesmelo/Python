a81cd9dcce80a8b2313971e4dc8be96439c53f7e
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-04-16 11:17:49 -0400

    update

--- ex092.py
+++ ex092.py
@@ -3,4 +3,11 @@ dados = dict()
 dados['nome'] = str(input('Nome: '))
 nasc = int(input('Ano de Nascimento: '))
 dados['idade'] = datetime.now().year - nasc
-dados['ctps'] = int(input('Carteira de Trabalho'))
+dados['ctps'] = int(input('Carteira de Trabalho (0 não tem): '))
+if dados['ctps'] != 0:
+    dados['contratação'] = int(input('Ano de Contratação: '))
+    dados['salário'] = float(input('Salário: R$'))
+    dados['aposentadoria'] = dados['idade'] + ((dados['contratação'] + 35) - datetime.now().year)
+print('-=' * 30)
+for k, v in dados.items():
+    print(f'  -{k} tem o valor {v}')
