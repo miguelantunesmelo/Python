87a278dc7248be863bcf16fea5141d8df4838287
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-04-16 10:47:35 -0400

    update

--- ex091.py
+++ ex091.py
@@ -2,7 +2,6 @@ from random import randint
 jogo = { 'jogador1': randint(1, 6),
          'jogador2': randint(1, 6),
          'jogador3': randint(1, 6),
-         'jogador4': randint(1, 6)
+         'jogador4': randint(1, 6)}
 
 
-}
