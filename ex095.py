db762e0e439fd938f4c82146804a889a139c0186
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-04-16 16:13:43 -0400

    update

--- ex095.py
+++ ex095.py
@@ -19,10 +19,15 @@ while True:
         print('ERRO! Responda apenas S ou N.')
     if resp == 'N':
          break
+print('-=' * 30)
+print('cod ', end='')
+for i in jogador.keys():
+    print(f'{i:<15}', end='')
+print()
 print('-' * 40)
 for k, v in enumerate(time):
-    print(f'{k:>4}', end='')
+    print(f'{k:>3} ', end='')
     for d in v.values():
-        print(f'{str(d):<15}', end='')
+        print(f'{str(d):<15} ', end='')
     print()
 print('-' * 40)
