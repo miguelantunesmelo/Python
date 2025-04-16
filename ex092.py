073d125b9d249a161705b45092ff487cdc801d56
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-04-16 11:05:11 -0400

    new exercise

---
+++ ex092.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+dados = dict()
+dados['nome'] = str(input('Nome: '))
+nasc = int(input('Ano de Nascimento: '))
+dados['idade'] = datetime.now().year - nasc
+dados['ctps'] = int(input('Carteira de Trabalho'))
