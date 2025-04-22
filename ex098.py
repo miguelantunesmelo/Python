49130744c215bdae242e39788a17659466087385
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-04-22 13:56:20 -0400

    update

--- ex098.py
+++ ex098.py
@@ -1,11 +1,37 @@
-def contador(i, f, p):
-    print(f'Contagem de {i} até {f} de {p} em {p}')
+from time import sleep
 
-    cont = i
-    while cont <= f:
-        print(f'{cont} ', end='')
-        cont += p
-    print('FIM!')
+
+def contador(i, f, p):
+    if p < 0:
+        p *= -1
+    if p == 0:
+        p = 1
+    print('-=' * 20)
+    print(f'Contagem de {i} até {f} de {p} em {p}')
+    sleep(2.5)
+
+    if i < f:
+        cont = i
+        while cont <= f:
+            print(f'{cont} ', end='', flush=True)
+            sleep(0.5)
+            cont += p
+        print('FIM!')
+    else:
+        cont = i 
+        while cont >= f:
+            print(f'{cont} ', end='', flush=True)
+            sleep(0.5)
+            cont -= p
+        print('FIM!')
+    
 
 # Programa principal
-contador(1, 10, 1)
+contador(0, 100, 10)
+contador(10, 0, 2)
+print('-=' * 20)
+print('Agora é a sua vez de personalizar a contagem!')
+ini = int(input('Início: '))
+fim = int(input('Fim:    '))
+pas = int(input('Passo:  '))
+contador(ini, fim, pas)
