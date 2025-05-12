e588291c21d07b7f9b98a8c467f588880a57bfd3
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-05-12 11:00:59 -0400

    new update

---
+++ ex102.py
@@ -0,0 +1,17 @@
+def fatorial(n, show=False):
+    f = 1 
+    for c in range(n, 0, -1):
+        if show:
+            print(c, end='')
+            if c>1:
+                print(' x ', end='')
+            else: 
+                print(' = ', end='')
+        f *= c
+    return f
+
+
+# Programa Principal
+print(fatorial(5, show=False))
+
+          
