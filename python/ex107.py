6be5c8b93422b87458c91253100f41617c83058b
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-05-28 15:40:24 -0400

    updates

--- python/ex107.py
+++ python/ex107.py
@@ -3,3 +3,4 @@ import moeda
 p = float(input('Digite o preço: R$'))
 print(f'A metade de R${p} é {moeda.metade(p)}')
 print(f'O dobro de R${p} é {moeda.dobro(p)}')
+print(f'Aumentando 10%, temos R${moeda.aumentar(p, 10)}')
