f77944ac573dc9ec5f1985d2b8482477543ee4a9
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-05-28 15:57:54 -0400

    updates

--- python/moeda.py
+++ python/moeda.py
@@ -1,19 +1,22 @@
-def aumentar(preço, taxa):
+def aumentar(preço = 0, taxa = 0):
     res = preço + (preço * taxa/100)
     return res
 
 
-def diminuir(preço, taxa):
+def diminuir(preço = 0, taxa = 0):
     res = preço - (preço * taxa/100)
     return res
 
 
-def dobro(preço):
+def dobro(preço = 0):
     res = preço * 2
     return res
 
 
-def metade(preço):
+def metade(preço = 0):
     res = preço / 2 
     return res
 
+
+def moeda(preço = 0, moeda = 'R$'):
+    return f'{moeda}{preço}'.replace('.',',')
