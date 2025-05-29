af77b5d235e77561f3dbd4d0cee682e3588f51e9
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-05-29 10:32:22 -0400

    updates

--- python/ex111/ex111.py
+++
@@ -1,6 +0,0 @@
-from ex111.utilidadescev import moeda
-from ex111.utilidadescev import dado
-
-p = float(input('Digite o preço: R$'))
-moeda.resumo(p, 20, 12)
-
---
+++ python/ex111/main.py
@@ -0,0 +1,6 @@
+from utilidadescev import moeda
+from utilidadescev import dado
+
+p = float(input('Digite o preço: R$'))
+moeda.resumo(p, 35, 22)
+
