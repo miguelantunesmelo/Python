542985ea21e80686fb6d614ac8e7a2fb91194111
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-06-02 17:05:43 -0400

    updates

--- python/ex113.py
+++ python/ex113.py
@@ -5,6 +5,9 @@ def leiaInt(msg):
         except(ValueError, TypeError):
             print('\033[31mERRO: por favor, digite um número inteiro válido.\033[m')
             continue
+        except (KeyboardInterrupt):
+            print('\n\033[31mUsuário preferiu não digitar esse número.\033[m')
+            return 0
         else:
             return n
 
