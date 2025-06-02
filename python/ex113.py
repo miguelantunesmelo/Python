1be7a9eb0598eeecfae82be39e3b8986b91c6d24
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-06-02 16:57:49 -0400

    updates

--- python/ex113.py
+++ python/ex113.py
@@ -1,2 +1,13 @@
-num = int(input('Digite um valor: '))
+def leiaInt(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except(ValueError, TypeError):
+            print('ERRO: por favor, digite um número inteiro válido.')
+            continue
+        else:
+            return n
+
+
+num = leiaInt('Digite um valor: '))
 print(f'O valor digitado foi {num}')
