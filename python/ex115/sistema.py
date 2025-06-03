e7dfbf6b04daf47542e3b78f1b8484a29632767e
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-06-03 11:20:09 -0400

    updates

--- python/ex115/sistema.py
+++ python/ex115/sistema.py
@@ -1,13 +1,15 @@
 from lib.interface import *
+from time import sleep
 
 while True:
     resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova Pessoa', 'Sair do Sistema'])
     if resposta == 1:
-        print('Opção 1')
+        cabeçalho('Opção 1')
     elif resposta == 2:
-        print('Opção 2')
+        cabeçalho('Opção 2')
     elif resposta == 3:
-        print('Saindo do sistema... Até logo!')
+        cabeçalho('Saindo do sistema... Até logo!')
         break
     else:
-        print('ERRO! Digite uma opção válida!')
+        print('\033[31mERRO! Digite uma opção válida!\033[m')
+    sleep(2)
