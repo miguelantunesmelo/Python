972c9a3d2e271f99833feadb50cf49ce3636cedd
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-09-02 13:40:03 -0400

    new exercises

--- anhaguera/m01/aula02.py
+++ anhaguera/m01/aula02.py
@@ -1,8 +1,19 @@
-idade = 70
+# Bem- vindo a Máquina de Venda Automática de Ingressos de Cinema!
 
-if idade < 18:
-    print("Menor de idade")
-elif idade >= 18 and idade < 65:
-    print("Adulto")
+# Solicita a idade do cliente
+idade = int(input('POr favor, digite sua idade: '))
+
+# Verifica a idadde paraa sugestão de filmes
+if idade < 12:
+    print('Recomendamos o filme infantil FILME 1.')
+elif 12 <= idade < 18:
+    print('Recomendamos o filme adolescente FILME 2.')
 else:
-    print("Idoso")
+    print('Recomendamos o emocionante FILME 3.')
+
+# Verifica a disponibilidade de ingressos
+quantidade_ingressos = 0 # Suponha que haja 10 ingressos disponíveis
+if quantidade_ingressos > 0:
+    print('Ingressos estão disponíveis. Divirta-se no cinema!')
+else:
+    print('Desculpe, todos os ingressos estão esgotados para hoje.')
