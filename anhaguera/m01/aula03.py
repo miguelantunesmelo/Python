7abc81a50075af8bd9ceaaaed53f8e1de6d4f78b
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-09-03 10:03:24 -0400

    new

--- anhaguera/m01/aula03.py
+++ anhaguera/m01/aula03.py
@@ -1,9 +1,20 @@
-numero = int(input('Digite um número (ou 0 para sair): '))
+# Lista de filmes para classificação 
+filmes = ['Filme 1', 'Filme 2', 'Filme 3', 'Filme 4', 'Filme 5']
 
+# Mensagens de boas-vindas
+print('Bem-vindo à classificação de filmes!')
+print('Você tem cinco filmes para classificar.')
+print('Digite. '0' a qualquer momento parar.\n')
 
-while numero != 0:
-    if numero % 2 == 0:
-        print('O número é par.')
-    else:
-        print('O número é ímpar.')
-    numero = int(input('Digite um número (ou 0 para sair): '))
+# Loop para iterar sobre cada filme na lista
+for filme in filmes:
+    # Solicita a classifcação  ao usuário ~
+    classificacao == input(f'Como você classificaria {filme} de 1 a 5? (ou 0 para parar): ')
+
+    # Verifica se o usuário deseja parar
+    if classificacao == '0':
+        print('Que pena que você não irá classificar mais os filmes.')
+        break   # Encerra o loop principal
+
+    # Converte classificação 
+
