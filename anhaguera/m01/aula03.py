6912923d0b661e74f343af6e193ea5571fe770c2
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-09-03 09:51:43 -0400

    new

--- anhaguera/m01/aula03.py
+++ anhaguera/m01/aula03.py
@@ -1,8 +1,9 @@
-numero = int(input('Digite um número (ou 0 paara sair): '))
+numero = int(input('Digite um número (ou 0 para sair): '))
+
 
 while numero != 0:
     if numero % 2 == 0:
         print('O número é par.')
     else:
         print('O número é ímpar.')
-    numero = int(input('Digite um número (ou 0 patra sair): '))
+    numero = int(input('Digite um número (ou 0 para sair): '))
