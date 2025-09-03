819a43427478ee2ccf91483b0daafc9347b0e8db
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-09-03 13:57:29 -0400

    updates

--- anhaguera/m02/aula02.py
+++ anhaguera/m02/aula02.py
@@ -1,23 +1,11 @@
-# Criando o conjunto
-meu_conjunto = set()
-
-# Adicionando elementos ao conjunto
-meu_conjunto.add(10)
-meu_conjunto.add(20)
-meu_conjunto.add(30)
-
-# Imprimindo o conjunto
-print('Conjunto após adicionar elementos:', meu_conjunto)
-
-# Verificando se um elemento está no conjunto 
-elemento = 20
-if elemento in meu_conjunto:
-    print(f'{elemento} está no conjunto.')
-else:
-    print(f'{elemento} não está no conjunto.')
-
-# Removendo um elemento no conjunto 
-meu_conjunto.remove(20)
-
-# Imprimindo o conjunto atualizado
-print('Conjunto após remover o elemento 20:', meu_conjunto)
+# Importe as bibliotecas necessárias 
+import numpy as np
+# Dados dos partcipantes 
+participantes = [
+    {
+        'nome': 'Alice',
+        'localização': 'EUA',
+        'afiliação': 'Universidade A',
+        'interesses': ['Física', 'Astronomia']
+    }
+]
