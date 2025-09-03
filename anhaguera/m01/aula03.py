9228354c46d70ece8d044651324e8026c013cd59
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-09-03 10:13:17 -0400

    up

--- anhaguera/m01/aula03.py
+++ anhaguera/m01/aula03.py
@@ -4,12 +4,12 @@ filmes = ['Filme 1', 'Filme 2', 'Filme 3', 'Filme 4', 'Filme 5']
 # Mensagens de boas-vindas
 print('Bem-vindo à classificação de filmes!')
 print('Você tem cinco filmes para classificar.')
-print('Digite. '0' a qualquer momento parar.\n')
+print('Digite. 0 a qualquer momento parar.\n')
 
 # Loop para iterar sobre cada filme na lista
 for filme in filmes:
     # Solicita a classifcação  ao usuário ~
-    classificacao == input(f'Como você classificaria {filme} de 1 a 5? (ou 0 para parar): ')
+    classificacao = input(f'Como você classificaria {filme} de 1 a 5? (ou 0 para parar): ')
 
     # Verifica se o usuário deseja parar
     if classificacao == '0':
@@ -24,7 +24,7 @@ for filme in filmes:
         print('Por favor, digite uma classificação válida de 1 a 5.')
     else:
         # Exibe a classificação e passa para o próximo filme
-        print(f'Voçê classificou {filme} com {classificacao} estrelas.\n')
+        print(f'Você classificou {filme} com {classificacao} estrelas.\n')
 
 # Mensagens de agradecimento ao finalizar 
 print('Obrigado por classificar os filmes!')
