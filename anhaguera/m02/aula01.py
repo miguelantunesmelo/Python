cda2f8a3f2561dc0a8af75675bd69a8b2a2d53f0
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-09-03 13:32:25 -0400

    updates

--- anhaguera/m02/aula01.py
+++ anhaguera/m02/aula01.py
@@ -4,4 +4,13 @@ convidados = ('Alice', 'Bob', 'Carol', 'David', 'Eve')
 # Lista de confirmações
 confirmados =['Bob', 'David']
 
-#
+# Identificar quem ainda não confirmou
+nao_confirmados = [convidado for convidado in convidados if convidados not in confirmados]
+
+# Exibir os convidados que ainda não confirmaram 
+print('Convidados que ainda não confirmaram:')
+for pessoa in nao_confirmados:
+    print(pessoa)
+# Enviar lembretes aos não confirmados 
+
+print('\nEnviamdo lembretes para os convidados que ainda não confirmaram')
