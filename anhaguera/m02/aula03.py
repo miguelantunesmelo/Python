2882a304aa356d46678e4d74eeada80932122edc
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-09-08 11:41:00 -0400

    new

--- anhaguera/m02/aula03.py
+++ anhaguera/m02/aula03.py
@@ -12,4 +12,11 @@ class Veiculo:
         self.velocidade -= decremento
 
     def status(self):
-        
+        return f'Marca: {self.marca}, Modelo: {self.modelo}, Ano: {self.ano}, Velocidade: {self.velocidade} km/h'
+    
+class Carro(Veiculo):
+    def __init__(self, marca, modelo, ano, potencia):
+        super().__init__(marca, modelo, ano)
+        self.potencia = potencia
+
+    
