68fcc67021d433a8acfcc5e885b464aaadc1d2ad
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-09-08 11:37:41 -0400

    new

---
+++ anhaguera/m02/aula03.py
@@ -0,0 +1,15 @@
+class Veiculo:
+    def __init__(self, marca, modelo, ano):
+        self.marca = marca
+        self.modelo = modelo
+        self.ano = ano
+        self.velocidade = 0
+
+    def acelerar(self, incremento):
+        self.velocidade += incremento
+
+    def frear(self, decremento):
+        self.velocidade -= decremento
+
+    def status(self):
+        
