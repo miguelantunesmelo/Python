3c5b20094900dd2c626e4e3284cd0331f7b697ad
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-09-09 16:31:36 -0400

    datframe

---
+++ anhaguera/m03/aula03.py
@@ -0,0 +1,10 @@
+import pandas as pd 
+
+# Criando um DataFrame com 5 linhas de dados 
+data = {
+    'nome': ['Produto A', 'Produto B', 'Produto C', 'Produto A', 'Produto E'],
+    'quantidade de itens comprados': [3, 1, 4, 3, 2],
+    'tipo de item': ['Eletrônico', 'Vestuário', 'Alimento', 'Eletrônico' , 'Alimento'],
+    'receita total': [120, 80, 60, 120, 90]
+}
+
