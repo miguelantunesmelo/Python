0191c9c29c25f4904b3d2982e2607bd8fa595e33
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-09-09 11:04:05 -0400

    classificação livro

---
+++ anhaguera/m02/aula05.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+# Classe para representar um livro 
+class Livro:
+    def __init__(self, titulo, autor, ano_publicacao):
+        self.titulo = titulo
+        self.autor = autor
+        self.ano_publicacao = ano_publicacao
+
+    def __str__(self):
+        return f'{self.titulo} por {self.autor}, Publicado em {self.ano_publicacao}'
+    
