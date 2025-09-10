3529d8c8992532124fd2e7e6f201b9dfe5643c70
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-09-10 14:00:41 -0400

    builder load criado

--- anhaguera/m04/aula02.py
+++ anhaguera/m04/aula02.py
@@ -4,3 +4,21 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.tabbedpanel import TabbedPanel, TabbedPanelItem
 
+Builder.load_string('''
+<TabsLayout>:
+    TabbedPanel:
+        do_default_tab: False
+        TabbedPanelItem:
+            text: 'Tab 1'
+            BoxLayout:
+                orientation: 'vertical'
+                Label:
+                    text: 'Content for Tab 1'
+        TabbedPanelItem:
+            text: 'Tab 2'
+            BoxLayout:
+                orientation: 'vertical'
+                Label:
+                    text: 'Content for Tab 2'
+''')
+
