65af7367272c1fefd63c0e0c8138f0bbd5109508
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-09-10 11:03:20 -0400

    criado a tabela der funcionários

--- anhaguera/m03/aula05.py
+++ anhaguera/m03/aula05.py
@@ -2,3 +2,14 @@ import sqlite3
 # Passo 1: Conectar ao banco de dados SQLite (ou criá-lo se não existir)
 conn = sqlite3.connect('funcionarios.db')
 
+# Passo 2: Criar a tabela de funcionários
+cursor = conn.cursor()
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS funcionarios (
+        id INTEGER PRIMARY KEY,
+            nome TEXT,
+            cargo TEXT,
+            salario REAL 
+    )
+''')
+
