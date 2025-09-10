e5bc930d73277b829857f9115d9fb4ba77a1788c
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-09-10 11:52:09 -0400

    rota inical com flask

--- anhaguera/m04/aula01.py
+++ anhaguera/m04/aula01.py
@@ -3,3 +3,8 @@ from flask import Flask
 # Criação da aplicação Flask
 app = Flask(__name__)
 
+# Rota para a página inicial
+@app.route('/')
+def hello():
+    return 'Bem-vindo ao back-end simples com Flask!'
+
