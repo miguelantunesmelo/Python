729a224df544f3ed71b4892b44c6fc00bea08ad8
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-09-10 13:56:06 -0400

    importaç~~oes

---
+++ anhaguera/m04/aula02.py
@@ -0,0 +1,6 @@
+from kivy.app import App
+from kivy.lang import Builder 
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.tabbedpanel import TabbedPanel, TabbedPanelItem
+
