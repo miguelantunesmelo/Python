e74081cbdbf6a9135a4b9a2ae4dba9d48b76a0ae
Miguel Antunes Melo <miguelmelom777@gmail.com>
2025-09-10 11:50:46 -0400

    criação no flask

--- anhaguera/m04/aula01.py
+++ anhaguera/m04/aula01.py
@@ -1,15 +1,5 @@
-# Criando uma página HTML com Python
-html_code = """
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Exemplo de Front-end com Python</title>
-</head>
-<body>
-    <h1>Olá, mundo!</h1>
-    <p>Esta é uma página web criada usando Python.</p>
-</body>
-</html>
-"""
-from IPython.display import HTML
-HTML(html_code)
+from flask import Flask 
+
+# Criação da aplicação Flask
+app = Flask(__name__)
+
